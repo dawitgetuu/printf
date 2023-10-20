9c9026a36de1224405e9ef3f2567d24aad146e2e
dawit getuu <archfun3@gmail.com>
2023-10-20 00:45:17 -0700

    0x11. C - printf

--- main.h
+++ main.h
@@ -107,7 +107,7 @@ char buffer[],
 	int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
-int is_printable(char);
+int by getting size_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
 
@@ -115,4 +115,3 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
-
